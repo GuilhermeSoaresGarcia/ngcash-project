75bab93d627d53b3a904f1345ffd87c91de3a3c8
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-11-15 23:19:31 -0300

    feat: create a new user that uses an id of an automatic created new account

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,10 +1,16 @@
 import { public_Users } from '@prisma/client';
 import { prisma } from '../db/db';
-import IUser from '../interfaces/IUser';
+import { newAccount } from '../services/accountServices';
 
-async function newUser(obj: Omit<public_Users, 'id' | 'accountId'>) {
+async function newUser(obj: public_Users) {
+  const { username, password } = obj;
+  const { id } = await newAccount();
   const result = await prisma.public_Users.create({
-    data: obj
+    data: {
+      username,
+      password,
+      accountId: id,
+    }
   });
   return result;
 }
